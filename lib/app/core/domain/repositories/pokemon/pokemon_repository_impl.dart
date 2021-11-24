b5c88b46ed5306a44e25a7d73b69cd8899214133
ZianFahrudy <zianfahrudy@gmail.com>
2021-11-24 16:52:08 +0700

    add mutation request example

--- lib/app/core/domain/repositories/pokemon/pokemon_repository_impl.dart
+++ lib/app/core/domain/repositories/pokemon/pokemon_repository_impl.dart
@@ -52,4 +52,52 @@ class PokemonRepositoryImpl implements PokemonRepository {
       yield left(Failure(e.toString()));
     }
   }
+
+  ///THIS IS A SAMPLE FOR MAKING MUTABLE REQUEST
+  ///login
+  static String login(String email, String password) {
+    return '''
+            mutation{
+              login(data: {
+                input:"$email",
+                password: "$password"
+              }){
+                token
+                user{
+                  _id
+                  email
+                  phonenumber
+                  referralCode
+                  username
+                  emailVerified
+                }
+              }
+            }
+        ''';
+  }
+
+  // //Example on how to implement mutation
+  // Future<String> Login(String email, password) async {
+  //   try {
+  //     ///initializing GraphQLConfig
+  //     GraphQLConfig graphQLConfiguration = GraphQLConfig();
+  //     GraphQLClient _client = graphQLConfiguration.clientToQuery();
+  //     QueryResult result = await _client.mutate(
+  //       MutationOptions(
+  //         document: gql(login(email, password)), // this
+  //       ),
+  //     );
+  //     if (result.hasException) {
+  //       print(result.exception?.graphqlErrors[0]?.message);
+  //     } else if (result.data != null) {
+  //       //  parse your response here and return
+  //       // var data = User.fromJson(result.data["register"]);
+  //     }
+
+  //     return "";
+  //   } catch (e) {
+  //     print(e);
+  //     return "";
+  //   }
+  // }
 }
